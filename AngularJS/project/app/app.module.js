5c82f82c4273a09d692c3845ed2a8ee8df04d53f
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-04-26 13:33:12 +0300

    initial header component

--- AngularJS/project/app/app.module.js
+++ AngularJS/project/app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('ketApp', []);
+angular.module('ketApp', ['myHeader']);
---
+++ AngularJS/project/app/header/header.component.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.
+  module('myHeader').
+  component('myHeader', {
+    templateUrl: 'app/header/header.template.html'
+});
+  
