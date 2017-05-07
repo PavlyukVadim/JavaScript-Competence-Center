f6a21e17edbae1376461c4d8a927bf562b52c716
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-07 14:47:54 +0300

    add scroll to

--- AngularJS/project/app/app.module.js
+++ AngularJS/project/app/app.module.js
@@ -1,6 +1,7 @@
 'use strict';
 
-angular.module('ketApp', ['myHeader',
+angular.module('ketApp', ['duScroll',
+													'myHeader',
 													'ourServices',
 													'portfolio',
 													'achievements',
