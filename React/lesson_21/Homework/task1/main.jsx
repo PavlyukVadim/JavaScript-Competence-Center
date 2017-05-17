56e87ff75e8fd3eada3642226295b73091f6182a
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-05-17 06:41:16 +0300

    lesson_21

---
+++ React/lesson_21/Homework/task1/main.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Counter = React.createClass({
+  getInitialState: function() {
+    return {
+      count: 0
+    }
+  },
+
+  plusHandler: function() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+
+  minusHandler: function() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button className="btn-lg"
+                onClick={this.plusHandler}>+</button>
+        <button className="btn-lg"
+                onClick={this.minusHandler}>-</button>
+      </div>)
+  }
+})
+
+var container = document.getElementById('example');
+ReactDOM.render(<Counter/>, container);
